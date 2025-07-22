447bc236537f1ce348a1e1e7e08061cd2d6f80a6
NikolBallesteros30 <nikballes30@gmail.com>
2025-07-22 17:48:14 -0500

    Update ModalConfiguracion.vue

--- talonario/src/components/ModalConfiguracion.vue
+++ talonario/src/components/ModalConfiguracion.vue
@@ -1,34 +1,42 @@
 <template>
   <div class="modal">
     <div class="modal-box">
-      <h2>CONFIGURA TU TALONARIO</h2>
-      <input v-model="premio" placeholder="Ingrese el premio de la rifa" />
-      <input v-model="valor" placeholder="Ingrese valor de la boleta" />
-      <select v-model="loteria">
-        <option disabled value="">Seleccione la lotería</option>
-        <option>La Perla</option>
-        <option>Chance Millonario</option>
-      </select>
-      <select v-model="cantidadBoletas">
-        <option disabled value="">Cantidad de Boletas</option>
-        <option v-for="n in 200" :key="n">{{ n }}</option>
-      </select>
-      <input type="date" v-model="fecha" />
-      <button @click="guardar">Guardar</button>
+      <div class="modal-header">
+        <h2>CONFIGURA TU TALONARIO</h2>
+        <button class="close-btn" @click="cerrar">×</button>
+      </div>
+
+      <div class="modal-body">
+        <input v-model="premio" placeholder="Ingrese el premio de la rifa" />
+        <input v-model="valor" placeholder="Ingrese valor de la boleta" />
+        <select v-model="loteria">
+          <option disabled value="">Seleccione la lotería</option>
+          <option>La Perla</option>
+          <option>Chance Millonario</option>
+          <option>Cruz Roja</option>
+          
+        </select>
+        <select v-model="cantidadBoletas">
+          <option disabled value="">Cantidad de Boletas</option>
+          <option v-for="n in 200" :key="n">{{ n }}</option>
+        </select>
+        <input type="date" v-model="fecha" />
+        <button class="guardar-btn" @click="guardar">Guardar</button>
+      </div>
     </div>
   </div>
 </template>
 
+
 <script setup>
 import { ref } from 'vue'
+const emit = defineEmits(['guardar'])
 
 const premio = ref('')
 const valor = ref('')
 const loteria = ref('')
 const cantidadBoletas = ref('')
 const fecha = ref('')
-
-const emit = defineEmits(['guardar'])
 
 function guardar() {
   if (!premio.value || !valor.value || !loteria.value || !cantidadBoletas.value || !fecha.value) {
@@ -41,7 +49,12 @@ function guardar() {
     valor: valor.value,
     loteria: loteria.value,
     cantidadBoletas: cantidadBoletas.value,
-    fecha: fecha.value,
+    fecha: fecha.value
   })
 }
+
+function cerrar() {
+  alert("No puedes cerrar este modal sin completar los datos.")
+}
+
 </script>
