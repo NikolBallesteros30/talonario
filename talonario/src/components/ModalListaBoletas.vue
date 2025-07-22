4acf6c016c52ff5af0a53b02833d94368400c528
NikolBallesteros30 <nikballes30@gmail.com>
2025-07-22 17:49:23 -0500

    Update ModalListaBoletas.vue

--- talonario/src/components/ModalListaBoletas.vue
+++ talonario/src/components/ModalListaBoletas.vue
@@ -2,27 +2,39 @@
   <div class="modal">
     <div class="modal-box">
       <h3>Listado de Boletas</h3>
-      <table ref="tabla">
-        <thead>
-          <tr>
-            <th>#</th><th>Comprador</th><th>Contacto</th><th>Estado</th><th>Pagado</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="b in boletas.filter(b => b.estado !== 'disponible')" :key="b.numero">
-            <td>{{ b.numero }}</td>
-            <td>{{ b.comprador }}</td>
-            <td>{{ b.contacto }}</td>
-            <td>{{ b.estado }}</td>
-            <td>{{ b.pagado }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <button @click="descargarPDF">Descargar PDF</button>
-      <button @click="$emit('cerrar')">Cerrar</button>
+      <div class="tabla-wrapper" ref="tabla">
+        <table class="tabla-boletas">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Comprador</th>
+              <th>Contacto</th>
+              <th>Estado</th>
+              <th>Pagado</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="b in boletas.filter(b => b.estado !== 'disponible')" :key="b.numero">
+              <td>{{ b.numero }}</td>
+              <td>{{ b.comprador }}</td>
+              <td>{{ b.contacto }}</td>
+              <td>
+                <span :class="'estado-tag ' + b.estado">{{ b.estado }}</span>
+              </td>
+              <td>${{ b.pagado }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div class="acciones-tabla">
+        <button class="btn" @click="descargarPDF">Descargar PDF</button>
+        <button class="btn" @click="$emit('cerrar')">Cerrar</button>
+      </div>
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref } from 'vue'
